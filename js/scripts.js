056fe2d207e147b8f5f13936238f2c169df7905e
Elizabeth Green <elizabeth@bombbomb.com>
2016-09-26 21:02:30 -0600

    basic index file completed

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+$window = $(window);
+$('[data-type="background"]').each(function(){
+    $(this).css('background-attachment', 'fixed');
+    var $bgobj = $(this);
+    var speedDividend = $bgobj.data('speed');
+    $bgobj.css('background-repeat', 'no-repeat');
+
+    $(window).on('scroll resize load', function() {
+        var parallaxDistance = ($bgobj.height() + $window.height()) / speedDividend;
+
+        if($bgobj.data('bg-attach') === 'bottom') {
+            var zeroPointPercent = $bgobj.height() / ($window.height() + $bgobj.height());
+            var startPoint = -(parallaxDistance * zeroPointPercent);
+            var endPoint = startPoint + parallaxDistance;
+        } else {
+            var zeroPointPercent = $window.height() / ($window.height() + $bgobj.height());
+            var startPoint = parallaxDistance * zeroPointPercent;
+            var endPoint = startPoint - parallaxDistance;
+        }
+
+        var offset = $bgobj.offset().top;
+        var progress = ($bgobj.height() + $bgobj.offset().top - $window.scrollTop()) / ($window.height() + $bgobj.height());
+
+        if($bgobj.data('bg-attach') === 'bottom') {
+            var yPos = Math.floor((parallaxDistance * (1 - progress)) + startPoint);
+
+            $bgobj.css('background-position', 'center bottom ' + yPos + 'px');
+        } else {
+            var yPos = Math.floor((parallaxDistance * progress) + endPoint);
+            $bgobj.css('background-position', 'center '+ yPos + 'px');
+        }
+    });
+});
